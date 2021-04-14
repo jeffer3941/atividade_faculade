b140148a66eadb831be5b466e4d5b212b21e7e67
Jefferson Ferreira <48527533+jeffer3941@users.noreply.github.com>
2021-04-13 21:59:46 -0300

    inicio projeto

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-window.prompt('insira o valor')
+const diaMaximo = new Date("2021-03-18");
+const dataAtual = new Date();
+let reserva = false;
+var teste = window.prompt('Deseja fazer uma reserva no hotel ?'); 
+var x = []
+if(teste == 'sim') {
+    if(dataAtual > diaMaximo) {
+        alert('data ultrapassada');
+    }
+    reserva = true
+}
