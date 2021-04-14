eec428b8580c94eaf3fbc4f741176e07afa13dad
Jefferson Ferreira <48527533+jeffer3941@users.noreply.github.com>
2021-04-13 23:24:50 -0300

    conclusao

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-const diaMaximo = new Date("2021-03-18");
-const dataAtual = new Date();
-let reserva = false;
-var teste = window.prompt('Deseja fazer uma reserva no hotel ?'); 
-var x = []
-if(teste == 'sim') {
-    if(dataAtual > diaMaximo) {
-        alert('data ultrapassada');
+const dateLimit = {day:18, month:03, year:2021};
+var dataUsers = [];
+const compareDate = ( dataReserved = String)=>{
+    let datefilter = dataReserved.split('/');
+
+    if(datefilter.length>0){
+        if(Number.parseInt(datefilter[2]) <= dateLimit.year){
+            if(Number.parseInt(datefilter[1]) <= dateLimit.month){
+               return Number.parseInt(datefilter[0]) < dateLimit.day;
+            }
+        }
     }
-    reserva = true
+    return false;
 }
+do{
+    let yourName = prompt('Seu Nome?');
+    let yourContact = prompt('Seu contato ?');
+   
+    let totalChildren = prompt('Quantas crianças ?');
+    let totalAdults = prompt('Quantas adultos ?');
+
+    let CorrentDate = prompt('Data da reserva?(ex. 6/12/2021)');
+    let reserved = false;
+
+
+    let totalPeopleCorrent = Number.parseInt(totalChildren)+Number.parseInt(totalAdults);
+    if(compareDate(CorrentDate)){
+        reserved = true;
+        alert('Reserva Realizada com Sucesso!');
+        alert(`---- INFORMAÇÃO ----
+               Nome: ${yourName} \n
+               Contato: ${yourContact} \n
+               Total de crianças: ${totalChildren} \n 
+               Adultos: ${totalAdults} \n 
+               Total de Pessoas: ${totalPeopleCorrent}`);
+    }else{
+        alert('Data não disponível!!');
+    }
+   
+    dataUsers.push({CorrentDate,yourName,yourContact,totalChildren,totalAdults, reserved, totalPeopleCorrent});
+}while (confirm('Deseja fazer sua reserva?!'));
